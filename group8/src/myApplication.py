c482c6c225e2781401bb27dd80281419ada3afe2
Marcus Greiff <marcusgreiff.93@hotmail.com>
2018-08-27 11:27:55 +0200

    Add group 8

--- group8/src/myApplication.py
+++ group8/src/myApplication.py
@@ -33,9 +33,9 @@ distances = { "front": 0.0, "left": 0.0, "right": 0.0, "rear": 0.0 };
 ################################################################################
 # This callback is triggered whenever there is a new distance reading coming in.
 def onDistance(msg, senderStamp, timeStamps):
-    print "Received distance; senderStamp=" + str(senderStamp)
-    print "sent: " + str(timeStamps[0]) + ", received: " + str(timeStamps[1]) + ", sample time stamps: " + str(timeStamps[2])
-    print msg
+    #print "Received distance; senderStamp=" + str(senderStamp)
+    #print "sent: " + str(timeStamps[0]) + ", received: " + str(timeStamps[1]) + ", sample time stamps: " + str(timeStamps[2])
+    #print msg
     if senderStamp == 0:
         distances["front"] = msg.distance
     if senderStamp == 1:
@@ -44,13 +44,30 @@ def onDistance(msg, senderStamp, timeStamps):
         distances["rear"] = msg.distance
     if senderStamp == 3:
         distances["right"] = msg.distance
+################### OUR CONST
 
+#lowerBound = numpy.array([33,80,40])
+#upperBound = numpy.array([102,255,255])
+lowerBound = numpy.array([35,170,40])
+upperBound = numpy.array([90,255,255])
+kernelOpen = numpy.ones((20,20))
+kernelClose = numpy.ones((20,20))
+#control constants for thrust
+referenceDistance = 0.40   # unit is meters
+Kp_thrust = 0.600/1.0
+Kd_thrust = 1.85/1.0 
+#control constants for steering
+referenceOffset = 320.0    #unit of measurte is pixels
+Kp_steer = 0.00055	
+
+################### END OUR CONST
 
 # Create a session to send and receive messages from a running OD4Session;
 # Replay mode: CID = 253
 # Live mode: CID = 112
 # TODO: Change to CID 112 when this program is used on Kiwi.
-session = OD4Session.OD4Session(cid=253)
+#session = OD4Session.OD4Session(cid=253)
+session = OD4Session.OD4Session(cid=112)
 # Register a handler for a message; the following example is listening
 # for messageID 1039 which represents opendlv.proxy.DistanceReading.
 # Cf. here: https://github.com/chalmers-revere/opendlv.standard-message-set/blob/master/opendlv.odvd#L113-L115
@@ -72,12 +89,20 @@ shm = sysv_ipc.SharedMemory(keySharedMemory)
 mutex = sysv_ipc.Semaphore(keySemCondition)
 cond = sysv_ipc.Semaphore(keySemCondition)
 
+#initialize error distance
+distance = 0.0
+steer = 0.0
+errorDistance = 0.0
+errorDistanceUS = 0.0
+wasTracking = False
+samples = 0
+
 ################################################################################
 # Main loop to process the next image frame coming in.
 while True:
     # Wait for next notification.
     cond.Z()
-    print "Received new frame."
+    #print "Received new frame."
 
     # Lock access to shared memory.
     mutex.acquire()
@@ -97,18 +122,101 @@ while True:
     # TODO: Add some image processing logic here.
 
     # The following example is adding a red rectangle and displaying the result.
-    cv2.rectangle(img, (50, 50), (100, 100), (0,0,255), 2)
+    # cv2.rectangle(img, (50, 50), (100, 100), (0,0,255), 2)
+
+################ OUR CODE #######################
+    #convert to HSV
+    imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #correct image to compendate for luminosity noise
+    h,s,v = cv2.split(imgHSV)
+    clahe = cv2.createCLAHE(clipLimit=2,tileGridSize=(8,8))
+    #h1 = clahe.apply(h)
+    s1 = clahe.apply(s)
+    v1 = clahe.apply(v)
+    imgCL = cv2.merge([h,s1,v1])
+    #mask color
+    mask=cv2.inRange(imgCL,lowerBound,upperBound)
+    ##cv2.imshow("mask",mask)   
+    #filter
+    maskOpen = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernelOpen)
+    maskClose = cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
+    ##cv2.imshow("maskOpen",maskOpen)
+    ##cv2.imshow("maskClose",maskClose)
+    #track color
+    im, conts, h = cv2.findContours(maskClose.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    w_max = 0
+    x = 0 
+    y = 0
+    h = 0
+    w = 0
+    targetDetected = False 
+    #loop over the found green regions and take only the biggest and square shaped
+    for i in range(len(conts)) :
+        ##cv2.drawContours(img,conts,-1,(255,0,0),3)
+        x_tmp,y_tmp,w_tmp,h_tmp=cv2.boundingRect(conts[i])
+        #if w_tmp>w_max and float(w_tmp)/float(h_tmp)<1.5 and float(w_tmp)/float(h_tmp)>0.5 :
+        if w_tmp>w_max  :
+            x = x_tmp 
+            y = y_tmp
+            h = h_tmp
+            w = w_tmp
+            w_max = w_tmp
+            targetDetected = True 
+
+    ##cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2) 
+    ##cv2.circle(img,(int(x+w/2),int(y+h/2)),int((w+h)/4),(0,0,255),2)
+
+    if (targetDetected and distance<2.0):
+        #keep old distance error
+        errorDistanceOld = errorDistance
+        #estimate distance from pixels
+        distancePX = 63.35*w**(-1.18)
+        distanceUS = distances["rear"]
+        if abs(distancePX-distanceUS)<0.20 and abs(steer)<0.3 :
+            distance = (distancePX+distanceUS)/2
+        else :
+            distance = distancePX
+        errorDistance = distance - referenceDistance
+        #estimate speed error
+        errorSpeed = errorDistance - errorDistanceOld
+        #angle error in pixels
+        errorAngle = referenceOffset - (x+w/2)
+        #compute control action
+        thrust = Kp_thrust*errorDistance + Kd_thrust*errorSpeed
+        if errorDistance<0 :
+            thrust = 2.0*thrust
+        if thrust>0.15 :
+            thrust = 0.15
+        #steering control (all on pixels)
+        steer = Kp_steer*errorAngle*numpy.sign(thrust)
+        steer = min(max(steer,-0.66), 0.66)
+        ##cv2.circle(img,(int(320+steer*30),int(240-thrust*150)),20,(0,255,0),2)
+        ##print(distanceUS,distancePX,steer,thrust)
+        samples = 0
+        wasTracking = True
+    elif wasTracking and samples<3:
+        samples += 1
+    else : 
+        errorDistanceOld = 0.0
+        distance = 0.0
+        thrust = 0.0
+        steer = 0.0
+        wasTracking = False
+        samples = 0
+    
+############################################################################
+    # Example: Accessing the distance readings.
+    #print "Front = " + str(distances["front"])
+    #print "Left = " + str(distances["left"])
+    #print "Right = " + str(distances["right"])
+    #print "Rear = " + str(distances["rear"])
+
+##################################################
 
     # TODO: Disable the following two lines before running on Kiwi:
-    cv2.imshow("image", img);
-    cv2.waitKey(2);
-
-    ############################################################################
-    # Example: Accessing the distance readings.
-    print "Front = " + str(distances["front"])
-    print "Left = " + str(distances["left"])
-    print "Right = " + str(distances["right"])
-    print "Rear = " + str(distances["rear"])
+    ##cv2.imshow("image", img);
+    ##cv2.waitKey(2);
 
     ############################################################################
     # Example for creating and sending a message to other microservices; can
@@ -124,13 +232,13 @@ while True:
     #
     # Uncomment the following lines to steer; range: +38deg (left) .. -38deg (right).
     # Value groundSteeringRequest.groundSteering must be given in radians (DEG/180. * PI).
-    #groundSteeringRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_GroundSteeringRequest()
-    #groundSteeringRequest.groundSteering = 0
-    #session.send(1090, groundSteeringRequest.SerializeToString());
+    groundSteeringRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_GroundSteeringRequest()
+    groundSteeringRequest.groundSteering = steer
+    session.send(1090, groundSteeringRequest.SerializeToString());
 
     # Uncomment the following lines to accelerate/decelerate; range: +0.25 (forward) .. -1.0 (backwards).
     # Be careful!
-    #pedalPositionRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_PedalPositionRequest()
-    #pedalPositionRequest.position = 0
-    #session.send(1086, pedalPositionRequest.SerializeToString());
+    pedalPositionRequest = opendlv_standard_message_set_v0_9_6_pb2.opendlv_proxy_PedalPositionRequest()
+    pedalPositionRequest.position = thrust
+    session.send(1086, pedalPositionRequest.SerializeToString());
 
